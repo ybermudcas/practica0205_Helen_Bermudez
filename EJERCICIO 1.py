17d4612fcd37e9f4999e06a944c5e59ebec51603
ybermudcas <ybermudcas@educacion.navarra.es>
2023-11-16 15:01:20 +0100

    EJERCICIO 1

---
+++ EJERCICIO 1.py
@@ -0,0 +1,9 @@
+def saludo(nombre):
+    """Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+    Parámetros:
+    nombre= nombre que quiera introducir el usuario
+    """
+
+    print("Hola", nombre)
+    return
+
